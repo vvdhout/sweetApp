99846a1a0e2e317536c1b96eb86515f69f578d73
vvdhout <valentijnvdh@gmail.com>
2018-12-26 19:41:04 +0100

    SC: Constructor, basic mappings, fruit creation, fruit getter + initial truffle tests.

--- contracts/tokenContract.sol
+++ contracts/tokenContract.sol
@@ -1,17 +1,61 @@
+// This is the sweetApp contract. The only funcionality? Create a sweet-fruit token with a name and allow transfer between addresses.
+// These are the fruits that can be chosen from: Banana, Orange, Strawberry, and a Potato just for the heck of it.
+
 pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
 contract tokenContract is ERC721 {
 
-    address owner;
+    address private owner;
+    
+    // Fruit struct
+    struct fruit {
+        string typeOfFruit;
+        string name;
+    }
+    
+    // Track how many tokens exist and use as TokenId setter
+    uint256 public tokenCount;
+    
+    // Map a tokenId to a fruit struct
+    mapping (uint256 => fruit) fruits;
+    
+    // Map a fruithash to an existence boolean
+    mapping (bytes32 => bool) fruitExists;
+    
 
     constructor() public {
         owner = msg.sender;
+        tokenCount = 0;
+    }
+    
+    // Check if the fruit is one of the allowed types
+    modifier isSweet(string _fruit) {
+        require(keccak256(abi.encodePacked(_fruit)) == keccak256(abi.encodePacked("Banana")) || keccak256(abi.encodePacked(_fruit)) == keccak256(abi.encodePacked("Orange")) || keccak256(abi.encodePacked(_fruit)) == keccak256(abi.encodePacked("Strawberry")) || keccak256(abi.encodePacked(_fruit)) == keccak256(abi.encodePacked("Potato")), "Fruit type has to be a Banana, Orange, Strawberry, or Potato (we'll allow this diversion).");
+        _;
+    }
+    
+    // Let the users create their own fruits
+    function createFruit(string _typeOfFruit, string _name) public isSweet(_typeOfFruit) {
+        // Require that the fruit and name combo does not already exist
+        require(fruitExists[keccak256(abi.encodePacked(_typeOfFruit, _name))] == false, "Fruit and name combo already exists.");
+        // Require that name length is less than 30 characters
+        require(bytes(_name).length <= 30, "Name length has to be less than or equal to 30 characters.");
+        // Set existence for our fruithash to true.
+        fruitExists[keccak256(abi.encodePacked(_typeOfFruit, _name))] == true;
+        // Increase token count by one
+        tokenCount++;
+        // Map tokenId to a fruit
+        fruits[tokenCount].typeOfFruit = _typeOfFruit;
+        fruits[tokenCount].name = _name;
+        // // Mint the token
+        // _mint(msg.sender, tokenCount);
     }
 
-    function getContractOwner() public view returns (address) {
-        return owner;
+    // Get fruit info using tokenId
+    function getFruit(uint256 _tokenId) public view returns (string _name, string _typeOfFruit) {
+        return(fruits[_tokenId].name,fruits[_tokenId].typeOfFruit);
     }
 
 }
--- test/tokenContractTest.js
+++ test/tokenContractTest.js
@@ -1,16 +1,27 @@
 const tokenContract = artifacts.require("tokenContract");
 
-contract("tokenContract test", async accounts => {
+contract("sweetApp test", async accounts => {
 
-  it("should do this", async () => {
+  it("should allow us to create a fruit", async () => {
     // Get deployed contract
     let instance = await tokenContract.deployed();
     // Perform a function of the contract
-    await instance.function1(arg1, arg2, {from: accounts[0]});
+    await instance.createFruit("Banana", "Bork-fork the Banana Champ", {from: accounts[0]});
     // Store the result of another function of the contract
-    let result = await instance.function2(arg1, arg2);
+    let result = await instance.getFruit(1);
     // Assert if result is equal to something, and if not send a message
-    assert.equal(result, "equal this", "it did not do this");
+    assert.equal(result[0], "Bork-fork the Banana Champ", "name of the fruit was not equal to 'Bork-fork the Banana Champ'");
+    assert.equal(result[1], "Banana", "type of fruit was not equal to 'Banana'");
+  })
+
+  it("should allow us to get a fruit using tokenId", async() => {
+    // Get deployed contract
+    let instance = await tokenContract.deployed();
+    // Store the result of another function of the contract
+    let result = await instance.getFruit(1);
+    // Assert if result is equal to something, and if not send a message
+    assert.equal(result[0], "Bork-fork the Banana Champ", "name of the fruit was not equal to 'Bork-fork the Banana Champ'");
+    assert.equal(result[1], "Banana", "type of fruit was not equal to 'Banana'");
   })
 
 });
